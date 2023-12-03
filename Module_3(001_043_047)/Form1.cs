d42d4682352e4fa52ab0e98798365bb1046d9d4b
unknown <zee.mj26@hotmail.com>
2023-12-03 16:31:58 +0500

    Module3

---
+++ Module_3(001_043_047)/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.Show();
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection fon = new SqlConnection("Data Source =.; Initial Catalog=Credentials; Integrated Security = True");
+            string query = "Select * FROM Credentials where Username='" + textBox1.Text + "' AND Password = '" + textBox2.Text + "'";
+            fon.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(query, fon);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows.Count > 0)
+            {
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Try to enter Correct credentials...", "Info", MessageBoxButtons.OKCancel);
+            }
+        }
+    }
+}
---
+++ Module_3(001_043_047)/Form3.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text =="")
+            {
+                MessageBox.Show("Fill mandatory fields");
+            }
+            else if (textBox2.Text != textBox3.Text)
+            {
+                MessageBox.Show("Passwords donot match", " Help", MessageBoxButtons.RetryCancel);
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection("Data Source=Desktop-30389UE;Initial Catalog=Credentials;Integrated Security=True;Encrypt=False;");
+                SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Credentials]\r\n           ([Username]\r\n ,[Password])\r\n  VALUES\r\n " +
+                    "     ('" + textBox1.Text + "' ,'" + textBox2.Text + "')", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                //    con.Close();
+
+                MessageBox.Show("Data submitted successfully");
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
